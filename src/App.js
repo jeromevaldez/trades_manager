046aead6b59037ad84677051556522582532aa1b
Jerome Valdez <916377+jeromevaldez@users.noreply.github.com>
2025-02-08 11:36:05 -0800

    installing react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// filepath: /Users/jeromevaldez/Documents/github_repos/trades_manager/src/App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Just a test. Learning VS Code and Github. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            <p>Let's do another change here to look at how diffs work. Neat!</p>
+            <h2>A change to prove I can edit in a new features branch and then merge to the main one</h2>
+        </div>
+    );
+}
+
+export default App;
